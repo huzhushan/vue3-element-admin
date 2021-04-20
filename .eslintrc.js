791cf1c476023659c3819e0b7eb074a6cca08b99
huzhushan <huzhushan@126.com>
2021-04-20 10:51:28 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,25 +5,25 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    "parser": "babel-eslint",
-    "sourceType": 'module',
-    "ecmaVersion": 2020,
+    parser: 'babel-eslint',
+    sourceType: 'module',
+    ecmaVersion: 2020,
   },
   rules: {
-    "no-console": 0,
+    'no-console': 0,
     'no-use-before-define': 'off',
-    "no-unused-vars": [
-      'warn',
+    'no-unused-vars': [
+      'error',
       {
         // 允许声明未使用变量
-        "vars": "local",
+        vars: 'local',
         // 参数不检查
-        "args": "none"
-      }
+        args: 'none',
+      },
     ],
-    'vue/no-unused-vars': 'warn',
+    'vue/no-unused-vars': 'error',
     'no-prototype-builtins': 'off',
     'space-before-function-paren': 'off',
     'vue/custom-event-name-casing': 'off',
@@ -38,4 +38,4 @@ module.exports = {
     'vue/no-unused-components': 'warn',
     'vue/no-setup-props-destructure': 'off',
   },
-};
+}
