2a6d0d9630779fb0e7832fe6bbc7fcd45862364b
huzhushan <huzhushan@126.com>
2021-04-22 09:28:35 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@
  * @version:
  * @Date: 2021-04-20 11:06:21
  * @LastEditors: huzhushan@126.com
- * @LastEditTime: 2021-04-21 16:22:01
+ * @LastEditTime: 2021-04-22 09:28:15
  * @Author: huzhushan@126.com
  * @HomePage: https://huzhushan.gitee.io/vue3-element-admin
  * @Github: https://github.com/huzhushan/vue3-element-admin
@@ -33,7 +33,7 @@ export default env => {
   // console.log(111, env);
 
   return defineConfig({
-    base: '/vue3-element-admin-site/',
+    // base: '/vue3-element-admin-site/',
     plugins: [
       vue(),
       viteMockServe({
