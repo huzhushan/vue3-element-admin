7a809fa35bbcbe7373192434b80051d0d18767c0
huzhushan <huzhushan@126.com>
2021-04-28 10:33:25 +0800

    update

--- src/components/SvgIcon/index.vue
+++ src/components/SvgIcon/index.vue
@@ -18,7 +18,7 @@
  * @version: 
  * @Date: 2021-04-20 11:06:21
  * @LastEditors: huzhushan@126.com
- * @LastEditTime: 2021-04-21 12:45:50
+ * @LastEditTime: 2021-04-28 10:32:28
  * @Author: huzhushan@126.com
  * @HomePage: https://huzhushan.gitee.io/vue3-element-admin
  * @Github: https://github.com/huzhushan/vue3-element-admin
@@ -37,17 +37,13 @@ import { defineComponent, computed } from 'vue'
 export default defineComponent({
   name: 'SvgIcon',
   props: {
-    prefix: {
-      type: String,
-      default: 'icon',
-    },
     name: {
       type: String,
       required: true,
     },
   },
   setup(props) {
-    const symbolId = computed(() => `#${props.prefix}-${props.name}`)
+    const symbolId = computed(() => `#icon-${props.name}`)
     return { symbolId }
   },
 })
